f6f768758ba60f27440deae74f0bac8cdf7d3c84
Amine4jh <amine4jh@gmail.com>
2025-05-24 04:42:30 +0100

    Add 2-arguments.js File

---
+++ 2-arguments.js
@@ -0,0 +1,13 @@
+function myFunction(arg) {
+    if (arg === 0) {
+        console.log("No argument");
+    } else if (arg === 1) {
+        console.log("Argument found");
+    } else {
+        console.log("Arguments found");
+    }
+}
+
+myFunction(process.argv.length - 2)
+myFunction(process.argv.length - 1)
+myFunction(process.argv.length - 0)
