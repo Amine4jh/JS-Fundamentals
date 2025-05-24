716debde4b0e9add4e2754ebe53cc1f0a8ab589d
Amine4jh <amine4jh@gmail.com>
2025-05-24 18:35:27 +0100

    Fix Output Error

--- 2-arguments.js
+++ 2-arguments.js
@@ -1,13 +1,11 @@
 function myFunction(arg) {
-    if (arg === 0) {
-        console.log("No argument");
-    } else if (arg === 1) {
-        console.log("Argument found");
-    } else {
-        console.log("Arguments found");
-    }
+  if (arg === 0) {
+    console.log("No argument");
+  } else if (arg === 1) {
+    console.log("Argument found");
+  } else {
+    console.log("Arguments found");
+  }
 }
 
-myFunction(process.argv.length - 2)
-myFunction(process.argv.length - 1)
-myFunction(process.argv.length - 0)
+myFunction(process.argv.length - 2);
