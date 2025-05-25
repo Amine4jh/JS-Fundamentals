9941f32d6d2fd6bc4f50b822f0cd07fd628faa9a
Amine4jh <amine4jh@gmail.com>
2025-05-25 16:08:16 +0100

    Add 8-square.js File

---
+++ 8-square.js
@@ -0,0 +1,10 @@
+const arg = process.argv[2]
+const char = "x"
+
+if (isNaN(arg)) {
+    console.log("Missing size");
+} else {
+    for (let i = 0; i < arg; i++) {
+        console.log(char.repeat(arg));
+    }
+}
