69967e5a453a59cd6320470701c88d79dd01fe1f
Amine4jh <amine4jh@gmail.com>
2025-05-25 16:09:52 +0100

    Fix X Error

--- 8-square.js
+++ 8-square.js
@@ -1,10 +1,10 @@
-const arg = process.argv[2]
-const char = "x"
+const arg = process.argv[2];
+const char = "X";
 
 if (isNaN(arg)) {
-    console.log("Missing size");
+  console.log("Missing size");
 } else {
-    for (let i = 0; i < arg; i++) {
-        console.log(char.repeat(arg));
-    }
+  for (let i = 0; i < arg; i++) {
+    console.log(char.repeat(arg));
+  }
 }
